86df43a49d1aa35b80c8cf50b66c0cc1e22d155c
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-21 22:30:12 +0300

    [UPDATED] Complete template for events-log

--- src/components/orderEvent.js
+++ src/components/orderEvent.js
@@ -1,8 +1,20 @@
-const OrderEvent = () => {
+import { Col, Container } from "react-bootstrap";
+import { log } from '../assets/style'
+
+const OrderEvent = ({event}) => {
     return ( 
-        <div>
-            orderEvent
-        </div>
+        <Col md="3">
+            <Container style={log}>
+                    {
+                        event.map(({date,comment,creator})=>   
+                            <div key="index" className="m-2 p-2 border">
+                                <b><h6>{ date +  ', ' + creator }</h6></b>
+                                <span>{ comment }</span>
+                            </div>
+                        )
+                    }
+            </Container>
+        </Col>
      );
 }
  
