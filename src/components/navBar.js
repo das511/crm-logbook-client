0ebef6517b0423d586f5b94233ff89a945ea1ab3
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-21 23:18:11 +0300

    [UPDATED] Add icon for sign in/up

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,9 +1,12 @@
 import { React, useContext} from 'react';
 import { Context } from '../index';
-import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Navbar, Container, Nav, Image } from 'react-bootstrap'
 import { ADMIN_ROUTE, LOGIN_ROUTE, ORDER_ROUTE } from '../utils/consts';
 import { observer } from 'mobx-react-lite'
 import { NavLink } from 'react-router-dom';
+import person from '../assets/img/person-outline.svg'
+import { icon } from '../assets/style';
+import people from '../assets/img/people-outline.svg'
 
 const NavBar = observer(() => {
     const { user } = useContext(Context)
@@ -16,11 +19,17 @@ const NavBar = observer(() => {
                     <Nav className={ "ml-auto" }>
                         <NavLink to={ ORDER_ROUTE } className="m-1 btn btn-outline-light">Список заявок</NavLink>
                         <NavLink to={ ADMIN_ROUTE } className="m-1 btn btn-outline-light">Панель управления</NavLink>
-                        <NavLink to={ LOGIN_ROUTE } className="m-1 btn btn-outline-light" onClick={ () => user.setIsAuth(false) }>Выйти</NavLink>
+                        <NavLink to={ LOGIN_ROUTE } className="m-1 btn btn-outline-light" onClick={ () => user.setIsAuth(false) }>
+                            <Image src={ person } style={icon}/>
+                            Выйти
+                        </NavLink>
                     </Nav>
                     :
                     <Nav className={ "ml-auto" }>
-                        <NavLink to={ LOGIN_ROUTE } className="m-1 btn btn-outline-light" >Авторизация / Регистрация</NavLink>
+                        <NavLink to={ LOGIN_ROUTE } className="m-1 btn btn-outline-light" >
+                            <Image src={ people } style={icon}/>
+                            Авторизация / Регистрация
+                        </NavLink>
                     </Nav>
                 }
             </Container>
