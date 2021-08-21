671e71ff927e5673636200b5cfab087bda5188e9
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-21 22:30:39 +0300

    [UPDATED] Complete Item template

--- src/components/orderItem.js
+++ src/components/orderItem.js
@@ -1,6 +1,6 @@
 const OrderItem = ({order}) => {
     return ( 
-        <tr onClick={()=>console.log('clicked: '+order.id)}>
+        <tr onClick={()=>console.log('clicked: '+ order.id)}>
             <td>{ order.id }</td>
             <td>{ order.cratedAt }</td>
             <td>{ order.address }</td>
