88ccd1bc034183e8ed9c18e2ad85ad319ce9577d
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-25 20:49:48 +0300

    [UPDATAED] Added fetch oneOrder and event

--- src/pages/orderPage.js
+++ src/pages/orderPage.js
@@ -1,18 +1,26 @@
 import { observer } from 'mobx-react-lite';
-import { useContext } from 'react';
+import { useEffect, useState } from 'react';
 import { Container, Row, NavDropdown } from 'react-bootstrap'
+import { useParams } from 'react-router-dom';
 import OrderButton from '../components/orderButton';
 import OrderEvent from '../components/orderEvent';
-import OrderInfo from '../components/orderInfo';
-import { Context } from '../index'
+import OrderInfo from '../components/orderInfo'; 
+import { fetchOneOrder } from '../http/orderApi';
 
-const OrderPage = observer( (orderItem) => {
-    const { order } = useContext(Context)
+const OrderPage = observer( () => {
+    const [order, setOrder] = useState({})
+    const [event, setEvent] = useState({})
+    const {id} = useParams()
+    useEffect(() => {
+        fetchOneOrder(id).then(data=>setOrder(data))
+        //fetchEvent(id).then(data=>setEvent(data))
+    }, [null])
+
     return ( 
         <Container>
             <Row style={{ marginTop: 20}}>
-                <OrderInfo order={orderItem}/>
-                <OrderEvent event={order.event}/>
+                <OrderInfo order={order}/>
+                <OrderEvent event={event}/>
             </Row>
             <NavDropdown.Divider/>
             <Row>
