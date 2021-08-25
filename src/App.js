a213095e85524d7a3131b8167e731994dba296f6
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-25 20:47:21 +0300

    [UPDATED] Added full fetch

--- src/App.js
+++ src/App.js
@@ -6,20 +6,30 @@ import { Context } from ".";
 import AppRouter from "./components/appRouter";
 import NavBar from "./components/navBar";
 import { check } from "./http/userApi";
+import { fetchAll } from "./http/guideApi";
+import { fetchAllOrder } from "./http/orderApi";
 
 const App = observer(() => {
   const {user} = useContext(Context)
+  const {order} = useContext(Context)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     setTimeout(()=>{
+      fetchAll('status').then(data=>order.setStatus(data))
+      fetchAll('postType').then(data=>order.setPostType(data))
+      fetchAll('location').then(data=>order.setLocation(data))
+      fetchAll('nameZk').then(data=>order.setNameZk(data))
+
+      fetchAllOrder().then(data=>order.setOrders(data.rows))
+
       check().then(data=>{
         user.setUser(data)
         user.setIsAuth(true)
       }).finally(()=>setLoading(false))
     },1000)
     
-  }, [user])
+  }, [user,order])
  
   if(loading){
     return <Container className="d-flex justify-content-center align-items-center" style={{height: window.innerHeight - 54}}><div className="cp-spinner cp-round" ></div></Container>
