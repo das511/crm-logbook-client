8e6b5ea38b9235ad2532daee18f4165d988d664b
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-25 20:50:43 +0300

    [UPDATED] Added HTML markup

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import { Modal, Row, Button } from "react-bootstrap";
+import AdminLeftPanel from "../components/adminLeftPanel";
+import AdminRightPanel from "../components/adminRightPanel";
+
 const Admin = () => {
+    const [show,setShow] = useState(false)
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return ( 
-        <div>
-            Admin
-        </div>
+        <Row>
+            <AdminLeftPanel handleShow={handleShow} />
+            <AdminRightPanel/>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                </Button>
+                </Modal.Footer>
+            </Modal>
+        </Row>
      );
 }
  
