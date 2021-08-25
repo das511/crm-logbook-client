d519a507d997d4694361b31cf9bd777cd7b27dd0
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-25 16:17:12 +0300

    [UPDATED] fix createdAt

--- src/components/orderItem.js
+++ src/components/orderItem.js
@@ -6,7 +6,7 @@ const OrderItem = ({order}) => {
     return ( 
         <tr onClick={()=> history.push(ORDER_ROUTE + '/' + order.id)}>
             <td>{ order.id }</td>
-            <td>{ order.cratedAt }</td>
+            <td>{ order.createdAt }</td>
             <td>{ order.address }</td>
             <td>{ order.customer }</td>
             <td>{ order.contractor }</td>
