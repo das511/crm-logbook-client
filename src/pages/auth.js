ff498a39137616434f199b0dcda0c6037b1e8185
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-25 12:26:38 +0300

    [UPDATED] Added logick to all inputs and btn-s

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,11 +1,38 @@
+import { observer } from "mobx-react-lite";
+import { useContext, useState } from "react";
 import { Card, Container, Form, Button, Row, Col } from "react-bootstrap";
-import { NavLink, useLocation } from "react-router-dom";
-import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
+import { NavLink, useHistory, useLocation } from "react-router-dom";
+import { registration, logIn } from "../http/userApi";
+import { LOGIN_ROUTE, ORDER_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
+import { Context } from '../index'
 
-const Auth = () => {
+const Auth = observer(() => {
+    const {user} = useContext(Context)
     const location = useLocation()
+    const history = useHistory()
     const isLogin = location.pathname === LOGIN_ROUTE
-
+    const click = async () => {
+        try {
+            let data;
+            if (isLogin) {
+                data = await logIn(login,password)
+            }
+            else {
+                data = await registration(login,password,userRoleId,surname,firstName,lastName)
+            }
+            user.setUser(data)
+            user.setIsAuth(true)
+            history.push(ORDER_ROUTE)
+        } catch (error) {
+            alert(error.response.data.message);
+        }
+    }
+    const [login,setLogin] = useState('')
+    const [password, setPassword] = useState('')
+    const [surname, setSurname] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [userRoleId] = useState(1)
     return ( 
         <Container className="d-flex justify-content-center align-items-center" style={{height: window.innerHeight - 54}}>
             <Card className="w-50 p-5">
@@ -17,10 +44,15 @@ const Auth = () => {
                             <Form.Control
                                 placeholder="Введите номер телефона"
                                 className="mt-4"
+                                value={login}
+                                onChange={ e => setLogin(e.target.value)}
                             />
                             <Form.Control 
                                 placeholder="Введите пароль"
                                 className="mt-2"
+                                value={password}
+                                onChange={ e => setPassword(e.target.value)}
+                                type="password"
                             />
                         </Row>
                         :
@@ -28,26 +60,33 @@ const Auth = () => {
                             <Form.Control
                                 placeholder="Введите номер телефона"
                                 className="mt-4"
+                                value={login}
+                                onChange={ e => setLogin(e.target.value)}
                             />
                             <Form.Control 
                                 placeholder="Введите пароль"
                                 className="mt-2"
+                                value={password}
+                                onChange={ e => setPassword(e.target.value)}
                             />
                             <Form.Control 
                                 placeholder="Введите фамилию"
                                 className="mt-2"
+                                value={surname}
+                                onChange={ e => setSurname(e.target.value)}
                             />
                             <Form.Control 
                                 placeholder="Введите имя"
                                 className="mt-2"
+                                value={firstName}
+                                onChange={ e => setFirstName(e.target.value)}
                             />
                             <Form.Control 
                                 placeholder="Введите отчество"
                                 className="mt-2"
+                                value={lastName}
+                                onChange={ e => setLastName(e.target.value)}
                             />
-                            <Form.Select className="mt-2">
-                                <option disabled selected>Выберите должность</option>
-                            </Form.Select>
                         </Row>
 
                     }
@@ -60,9 +99,9 @@ const Auth = () => {
                         }
                         {
                             isLogin ?
-                            <Col xs="2"><Button className="btn-success">Войти</Button></Col>
+                            <Col xs="2"><Button onClick={click} className="btn-success">Войти</Button></Col>
                             :
-                            <Col xs="4"><Button className="btn-success">Зарегистрироваться</Button></Col>
+                            <Col xs="4"><Button onClick={click} className="btn-success">Зарегистрироваться</Button></Col>
                         }
                         
                     </Row>
@@ -70,6 +109,6 @@ const Auth = () => {
             </Card>
         </Container>
      );
-}
+})
  
 export default Auth;
