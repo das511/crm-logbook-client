d27db2505120dba48d54b1353becbd573db54ebc
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-25 12:20:21 +0300

    [UPDATED] add observer and useEffect and spin for loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import { observer } from "mobx-react-lite";
+import { useContext, useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
 import { BrowserRouter } from "react-router-dom";
+import { Context } from ".";
 import AppRouter from "./components/appRouter";
 import NavBar from "./components/navBar";
+import { check } from "./http/userApi";
 
-function App() {
+const App = observer(() => {
+  const {user} = useContext(Context)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(()=>{
+      check().then(data=>{
+        user.setUser(data)
+        user.setIsAuth(true)
+      }).finally(()=>setLoading(false))
+    },1000)
+    
+  }, [user])
+ 
+  if(loading){
+    return <Container className="d-flex justify-content-center align-items-center" style={{height: window.innerHeight - 54}}><div className="cp-spinner cp-round" ></div></Container>
+  }
+
   return (
     <BrowserRouter>
       <NavBar></NavBar>
       <AppRouter></AppRouter>
     </BrowserRouter>
   );
-}
+})
 
 export default App;
