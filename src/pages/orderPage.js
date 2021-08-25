8c8b929983bea0d77847ec86c1619ffafb0557d5
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-25 12:50:39 +0300

    [UPDATED] Added store, margin-top, oberver and items for component

--- src/pages/orderPage.js
+++ src/pages/orderPage.js
@@ -1,16 +1,20 @@
 import { observer } from 'mobx-react-lite';
-import { Container, Row } from 'react-bootstrap'
+import { useContext } from 'react';
+import { Container, Row, NavDropdown } from 'react-bootstrap'
 import OrderButton from '../components/orderButton';
 import OrderEvent from '../components/orderEvent';
 import OrderInfo from '../components/orderInfo';
+import { Context } from '../index'
 
-const OrderPage = observer( () => {
+const OrderPage = observer( (orderItem) => {
+    const { order } = useContext(Context)
     return ( 
         <Container>
-            <Row>
-                <OrderInfo/>
-                <OrderEvent/>
+            <Row style={{ marginTop: 20}}>
+                <OrderInfo order={orderItem}/>
+                <OrderEvent event={order.event}/>
             </Row>
+            <NavDropdown.Divider/>
             <Row>
                 <OrderButton/>
             </Row>
