5b7cce15e5676512c30225250d81e8f51977dd13
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-25 12:21:19 +0300

    [UPDATED] Added style for Logo and statusColor

--- src/assets/style.js
+++ src/assets/style.js
@@ -4,7 +4,24 @@ export const icon = {
     marginRight: 10
 }
 
+export const logo = {
+    width: 32, 
+    height: 32, 
+    marginRight: 10
+}
+
 export const log = {
-    height: 800,
+    height: 750,
+    overflowY: 'scroll'
+}
+
+export const scroll = {
+    height: 150,
     overflowX: 'scroll'
+}
+export const statusColor = (color) => {
+    return {
+        backgroundColor: color,
+        borderRadius: 10
+    }
 }
