3e3f9fc3d7c79d7f3a74340d7d3f41a4f8e4bb24
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-25 12:44:46 +0300

    [UPDATED] Added icon

--- src/pages/order.js
+++ src/pages/order.js
@@ -1,23 +1,29 @@
 import { observer } from "mobx-react-lite";
-import { Container, Row, Col, Table, Button } from "react-bootstrap";
+import { Container, Row, Col, Table, Button, Image } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 import OrderList from "../components/orderList";
-import { ORDER_HEADER } from "../utils/consts";
+import { ORDER_HEADER, ORDER_ROUTE } from "../utils/consts";
+import create from '../assets/img/create-outline.svg'
+import search from '../assets/img/search-outline.svg'
+import filter from '../assets/img/filter-outline.svg'
+import {icon} from '../assets/style'
 
 const Order = observer( () => {
+    const history = useHistory()
     return ( 
         <Container>
             <Row>
                 <Col className="m-3">
-                    <input type="submit" value="Добавить заявку" className="btn btn-success m-1"/>
+                    <Button className="m-1 btn-success" onClick={()=>history.push(ORDER_ROUTE + '/new')}><Image src={create} style={icon}/>Добавить заявку</Button>
                 </Col>
                 <Col className="m-3 text-end text-right">
-                    <Button type="button" className="btn btn-info m-1 text-light">Поиск</Button>
-                    <Button type="button" className="btn btn-info m-1 text-light">Фильтр</Button>
+                    <Button variant="outline-success" className="m-1"><Image src={search} style={icon}/>Поиск</Button>
+                    <Button variant="outline-success" className="m-1"><Image src={filter} style={icon}/>Фильтр</Button>
                 </Col>
             </Row>
             <Row>
-                <Table bordered hover>
-                    <thead>
+                <Table className="table-hover">
+                    <thead className="thead-default">
                         <tr>
                             { ORDER_HEADER.map(el=> <th key={el} >{ el }</th>) }
                         </tr>
