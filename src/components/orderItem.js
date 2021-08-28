1b73153f14ef90382fb9e2d22000c04c9ba6aaa9
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-28 10:47:18 +0300

    [UPDATED] Added color pricetag and find f/ user

--- src/components/orderItem.js
+++ src/components/orderItem.js
@@ -1,19 +1,23 @@
 import { useHistory } from "react-router-dom";
 import { ORDER_ROUTE } from "../utils/consts";
 import { Pricetag } from '../assets/img/pricetag'
+import { useContext } from "react";
+import { Context } from "..";
 
 const OrderItem = ({order}) => {
+    const {status} = useContext(Context).order
+    const {user} = useContext(Context).order
     const history = useHistory()
     return ( 
         <tr onClick={()=> history.push(ORDER_ROUTE + '/' + order.id)}>
             <td>
-                <Pricetag currentFill={ order.statusId } className="m-1"/>
-                { order.id }
+                <Pricetag currentFill={ status.find(s => order.statusId === s.id).color } className="m-1"/>
+                { order.id  }
             </td>
             <td>{ order.createdAt }</td>
             <td>{ order.address }</td>
             <td>{ order.customer }</td>
-            <td>{ order.userId }</td>
+            <td>{ user.find(u => order.userId === u.id).surname }</td>
             <td>{ order.comment }</td>
         </tr>
      );
