c2487a7556d407e6845906f81663bd7997ff9720
Kiry <Kiry@DESKTOP-DBBKC6K>
2021-08-28 10:46:41 +0300

    [UPDATED] REmove || '', Added find to guide values

--- src/components/orderInfo.js
+++ src/components/orderInfo.js
@@ -1,18 +1,21 @@
 import { Col, Container, Row, Image, Form } from "react-bootstrap";
 import { icon, scroll, statusColor } from "../assets/style";
 import tag from '../assets/img/pricetag-outline.svg'
+import { useContext } from "react";
+import { Context } from "..";
 
 const OrdrInfo = ({order}) => {
+    const {status,user,nameZk,postType,location} = useContext(Context).order
     return ( 
         <Col md="9">
             <Container>
                 <Form>
                     <Row>
                         <Col ms="auto">
-                            <h4 className="setion-title p-3" style={statusColor(order.statusId||"#d73d6c")}>
+                            <h4 className="setion-title p-3" style={statusColor( status.find(s => order.statusId === s.id)?.color ||"#d73d6c")}>
                                 <Image src={tag} style={icon}/>
                                 { order.id ? 'Заявка № ' + order.id + ' - ' : '' }
-                                <b> {order.statusId || 'Заявка создается'} </b>
+                                <b> {status.find(s => order.statusId === s.id)?.value || 'Заявка создается'} </b>
                             </h4>
                         </Col>
                     </Row>
@@ -21,23 +24,23 @@ const OrdrInfo = ({order}) => {
                             <Row className="mt-3" >   
                                 <Col>
                                     <Form.Label><b>Заявитель</b></Form.Label>
-                                    <Form.Control placeholder="ФИО" value={order.customer || ''} onChange={ e => {order.customer = e.target.value; console.log(order);}}/>
+                                    <Form.Control placeholder="ФИО" value={order.customer} onChange={ e => {order.customer = e.target.value;}}/>
                                 </Col>
                                 <Col>
                                     <Form.Label>&nbsp;</Form.Label>
-                                    <Form.Control placeholder="Телефон" value={order.phone || ''}/>
+                                    <Form.Control placeholder="Телефон" value={order.phone}/>
                                 </Col>
                             </Row>
                             <Row className="mt-3" >   
                                 <Col>
                                     <Form.Label><b>Адрес</b></Form.Label>
-                                    <Form.Control placeholder="Жилой массив" value={order.nameZkId || ''}/>
+                                    <Form.Control placeholder="Жилой массив" value={ nameZk.find(s => order.nameZkId === s.id)?.value}/>
                                 </Col>
                             </Row>
                             <Row className="mt-3" >
                                 <Col>
                                     <Form.Label>&nbsp;</Form.Label>
-                                    <Form.Control placeholder="Улица" value={order.address || ''}/>
+                                    <Form.Control placeholder="Улица" value={order.address}/>
                                 </Col>
                             </Row>
                         </Col>
@@ -45,23 +48,23 @@ const OrdrInfo = ({order}) => {
                             <Row className="mt-3">   
                                 <Col>
                                     <Form.Label><b>Исполнитель</b></Form.Label>
-                                    <Form.Control placeholder="Тип заявки" value={order.postTypeId || ''}/>
+                                    <Form.Control placeholder="Тип заявки" value={ postType.find(s => order.postTypeId === s.id)?.value}/>
                                 </Col>
                                 <Col>
                                     <Form.Label>&nbsp;</Form.Label>
-                                    <Form.Control placeholder="ФИО" value={order.userId || ''}/>
-                                </Col>
-                            </Row>
-                            <Row className="mt-3">   
-                                <Col>
-                                    <Form.Label>&nbsp;</Form.Label>
-                                    <Form.Control placeholder="Локация" value={order.locationId || ''}/>
+                                    <Form.Control placeholder="ФИО" value={user.find(s => order.userId === s.id)?.surname}/>
                                 </Col>
                             </Row>
                             <Row className="mt-3">   
                                 <Col>
                                     <Form.Label>&nbsp;</Form.Label>
-                                    <Form.Control placeholder="Назначенная дата" value={order.dateAppointed || ''}/>
+                                    <Form.Control placeholder="Локация" value={ location.find(s => order.locationId === s.id)?.value}/>
+                                </Col>
+                            </Row>
+                            <Row className="mt-3">   
+                                <Col>
+                                    <Form.Label>&nbsp;</Form.Label>
+                                    <Form.Control placeholder="Назначенная дата" value={order.dateAppointed} onChange={e=>{order.dateAppointed = e.target.value; console.log(order);}}/>
                                 </Col>
                             </Row>
                         </Col>
@@ -69,17 +72,17 @@ const OrdrInfo = ({order}) => {
                     <Row className="mt-3">
                         <Col md="12">
                             <Form.Label><b>Комментарий</b></Form.Label>
-                            <Form.Control as="textarea" rows={3} style={{resize:"none"}} value={order.comment || ''}/>
+                            <Form.Control as="textarea" rows={3} style={{resize:"none"}} value={order.comment}/>
                         </Col>
                     </Row>
                     <Row className="mt-3">
                         <Col>
                             <Form.Label><b>Фотографии до</b></Form.Label>
-                            <div style={scroll} className="border border-success p-2">{order.photoBefore || ''}</div>
+                            <div style={scroll} className="border border-success p-2">{order.photoBefore}</div>
                         </Col>
                         <Col>
                             <Form.Label><b>Фотографии после</b></Form.Label>
-                            <div style={scroll} className="border border-success p-2">{order.photoAfter || ''}</div>
+                            <div style={scroll} className="border border-success p-2">{order.photoAfter}</div>
                         </Col>
                     </Row>
                 </Form>
